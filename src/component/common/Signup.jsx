0ab6ca11393f0eed72af870e9d0f8275b764e762
Prasad <prasadlakhara@gmail.com>
2025-04-29 22:47:48 +0530

    SETUP

--- src/component/common/Signup.jsx
+++ src/component/common/Signup.jsx
@@ -81,7 +81,7 @@ function Signup() {
                     <div className="mb-3 password-wrapper">
                       <label for="parentconfirmpassword" className="form-label">Confirm Password</label>
                       <input type="password" className="form-control" id="parentconfirmpassword"/>
-                      <button type="button" className="toggle-password" onclick={togglePassword('parentconfirmpassword', this)}>
+                      <button type="button" className="toggle-password" onclick="togglePassword('parentconfirmpassword', this)">
                         <i className="ph ph-eye-slash"></i>
                       </button>
                     </div>
