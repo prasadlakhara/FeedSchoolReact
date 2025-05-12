de36d05422a069a50e84d3a351783d9d5c55e650
prasad <prasadlakhara@gmail.com>
2025-05-12 17:02:33 +0530

    Terms and condition page added

---
+++ src/component/TermsandConditions.jsx
@@ -0,0 +1,216 @@
+import TermsConditionHero from '../assets/img/Terms-Condition-Hero.svg';
+
+function TermsAndCondition() {
+    return (
+        <>
+            <section id="privacy-policy-hero-wrapper">
+                <div className="container my-auto">
+                    <div className="row">
+                        <div className="col-xl-6">
+                            <div className="privacy-policy-hero-content">
+                                <h2 className="text-primary">
+                                    Our Terms & Conditions<br />
+                                    <span className="text-secondary">Know your rights, responsibilities, and our commitments.</span>
+                                </h2>
+                                <p className="text-dark-200">
+                                    These Terms & Conditions outline the rules of engagement when using our platform ensuring safety, transparency, and a positive experience for everyone. Please read them carefully.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-xl-6 text-end">
+                            <div className="privacy-policy-hero-img">
+                                <img src={TermsConditionHero} alt="Privacy Policy Illustration" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="privacy-policy-content-wrapper">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-12 mb-4">
+                            <div className="privacy-policy-content-heading">
+                                <p className='text-primary fw-bold'>Effective Date: 1 January 2025</p>
+                            </div>
+                        </div>
+                        <div className="col-xl-6 terms-description-wrapper">
+                            <div className="top-description">
+                                <p>These Terms and Conditions ("Terms") govern your use of the website and services (collectively, the "Platform") provided by Feed My School, which is owned and operated by HDIT System ("Feed My School," "we," "our," or "us"). By accessing or using our Platform, you agree to comply with and be bound by these Terms. If you do not agree with these Terms, you are prohibited from using our Platform.
+                                    <br /><br />
+                                    Please read these Terms carefully before using our services.
+                                </p>
+                            </div>
+                            <div className="terms-description">
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        1. Acceptance of Terms
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p className='mb-0'>By accessing and using the Platform, you acknowledge that you have read, understood, and agree to be bound by these Terms, as well as any applicable laws and regulations. If you do not agree to these Terms, you must not use the Platform.</p>
+                                    </div>
+
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        2. User Registration and Account
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p>To access certain services, including placing lunch orders, you may be required to register an account with Feed My School. By registering an account, you agree to provide accurate, current, and complete information. You are solely responsible for maintaining the confidentiality of your account credentials (including your username and password), and for all activities that occur under your account.</p>
+                                        <p className='mb-0'>You agree to immediately notify us of any unauthorized use of your account or any other breach of security. We shall not be liable for any loss or damage arising from your failure to comply with this provision.</p>
+                                    </div>
+
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        3. Use of the Platform
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p className='mb-0'>You agree to use the Platform exclusively for lawful purposes and in accordance with these Terms. You may not use the Platform for any unlawful purpose, including but not limited to:</p>
+                                       <ul className='my-3'>
+                                            <li>Violating any applicable local, state, provincial, or federal laws.</li>
+                                            <li>Engaging in fraudulent or malicious activities.</li>
+                                            <li>Distributing harmful software, malware, or viruses.</li>
+                                            <li>Interfering with or disrupting the functionality of the Platform.</li>
+                                            <li>Harassing, threatening, or otherwise infringing upon the rights of others.</li>
+                                        </ul>
+                                        <p className="mb-0">Feed My School reserves the right, in its sole discretion, to suspend or terminate your access to the Platform if you violate any of these Terms or engage in unlawful conduct.</p>
+                                    </div>
+
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        4. Payment Terms
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p>By placing an order through the Platform, you agree to pay the total amount indicated for such orders, including applicable taxes and fees. Payment is processed through third-party payment processors, and you authorize Feed My School to charge your payment method for the orders you place. You agree to provide accurate and complete billing information and ensure that funds are available to cover the cost of the services.</p>
+                                        <p className="mb-0">Feed My School reserves the right to modify pricing and service fees at any time, with prior notice.</p>
+                                    </div>
+
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        5. Privacy and Data Protection
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p className='mb-0'>Your privacy is of paramount importance to us. By using the Platform, you consent to the collection, use, and processing of your personal information as outlined in our Privacy and Data Protection Policy. Please refer to our Privacy Policy to understand the scope of data collection and processing, and how we safeguard your personal information.</p>
+                                    </div>
+
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        6. Restrictions on Use
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p className='mb-0'>You shall not:</p>
+                                         <ul className='my-3'>
+                                            <li>Copy, modify, or distribute any part of the Platform without prior written consent from Feed My School.</li>
+                                            <li>Engage in any activity that could damage, disable, or impair the functionality of the Platform.</li>
+                                            <li>Use the Platform in any manner that could interfere with or disrupt other users' access or experience.</li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-6 terms-description-wrapper">
+                            <div className="terms-description">
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        7. Intellectual Property
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p className='mb-0'>
+                                            All content, materials, and intellectual property available through the Platform, including but not limited to text, graphics, logos, images, and software, are the property of Feed My School or its licensors and are protected by copyright, trademark, and other intellectual property laws. You are granted a limited, non-exclusive, non-transferable license to access and use the Platform for personal use only. Any unauthorized use of the Platform’s content or intellectual property is strictly prohibited.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        8. Limitation of Liability
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p>
+                                            To the fullest extent permitted by applicable law, Feed My School shall not be liable for any direct, indirect, incidental, special, or consequential damages arising from the use or inability to use the Platform, including but not limited to loss of data, profits, goodwill, or business opportunity, even if we have been advised of the possibility of such damages.
+                                            </p>
+                                        <p className='mb-0'>
+In no event shall Feed My School’s liability exceed the amount you paid for the service or product in question.                                            </p>
+                                    </div>
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        9. Indemnification
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p className='mb-0'>
+                                            You agree to indemnify, defend, and hold harmless Feed My School, HDIT System, and their respective officers, employees, agents, and affiliates, from and against any and all claims, liabilities, losses, damages, and expenses (including legal fees) arising out of your use of the Platform or your violation of these Terms.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        10. Termination
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p>
+                                            Feed My School reserves the right, in its sole discretion, to suspend or terminate your access to the Platform, without notice, for any violation of these Terms, any unlawful conduct, or any other reason deemed necessary by Feed My School. You may also terminate your account at any time by contacting us.
+                                            </p>
+                                        <p className='mb-0'>
+Upon termination, you must cease using the Platform, and any provisions that by their nature should survive termination shall remain in effect.                                            </p>
+                                    </div>
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        11. Modifications to the Terms
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p className='mb-0'>Feed My School reserves the right to update or amend these Terms at any time, with or without notice. All modifications will be posted on the Platform and will take effect immediately upon posting. Your continued use of the Platform after the changes have been posted constitutes your acceptance of the modified Terms.</p>
+                                    </div>
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        12. Governing Law and Dispute Resolution
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p className='mb-0'>These Terms shall be governed by and construed in accordance with the laws of [insert jurisdiction]. Any disputes arising from or related to these Terms or your use of the Platform shall be resolved in the courts of [insert jurisdiction], and you hereby consent to the exclusive jurisdiction and venue of such courts.</p>
+                                    </div>
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        13. Severability
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p className='mb-0'>If any provision of these Terms is found to be unlawful, void, or unenforceable, that provision shall be deemed severable from these Terms and shall not affect the validity and enforceability of the remaining provisions.</p>
+                                    </div>
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        14. Entire Agreement
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p className='mb-0'>These Terms constitute the entire agreement between you and Feed My School with respect to your use of the Platform and supersede all prior agreements, representations, and understandings, whether written or oral, relating to the subject matter of these Terms.</p>
+                                    </div>
+                                </div>
+                                <div className="terms-description-list">
+                                    <div className="terms-description-heading">
+                                        15. Contact Us
+                                    </div>
+                                    <div className="terms-description-content">
+                                        <p className='mb-0'>If you have any questions or concerns about this policy or how we handle your personal information, please contact us at:</p>
+                                        <div className="mt-4">
+                                            <p className='fw-bold mb-0'>Email: support@hdit.ca </p>
+                                            <p className='fw-bold mb-0'>Phone: [Insert Phone Number]</p>
+                                            <p className='fw-bold mb-0'>Address: [Insert Address]</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    );
+}
+
+export default TermsAndCondition;
